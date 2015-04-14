d3d687ca4d8e8fb4d39a41c2db347999f9023f4f
utilForever <utilForever@gmail.com>
2015-04-15 00:12:28 +0900

    Integrate delete Keyword Example

--- ModernCpp/deleteKeyword.cpp
+++ ModernCpp/deleteKeyword.cpp
@@ -1,5 +1,24 @@
+class DeleteTestCpp98
+{
+private:
+	int i;
+
+	DeleteTestCpp98(const DeleteTestCpp98&);
+
+public:
+	static void tempAssign(const DeleteTestCpp98& rhs)
+	{
+		DeleteTestCpp98 temp = rhs;
+	}
+
+	DeleteTestCpp98(int _i = 10) : i(_i) { }
+};
+
 class DeleteTestCpp11
 {
+private:
+	int i;
+
 public:
 	static void tempAssign(const DeleteTestCpp11& rhs)
 	{
@@ -8,13 +27,15 @@ public:
 
 	DeleteTestCpp11(int _i = 10) : i(_i) { }
 	DeleteTestCpp11(const DeleteTestCpp11&) = delete;
-
-private:
-	int i;
 };
 
 int main(int argc, char* argv [])
 {
+	// In C++98, declaring "tempAssign" functions private prevents clients from calling them.
+	// If code that still has access to them uses them, linking will fail due to missing function definitions.
+	DeleteTestCpp98 dtc98;
+	DeleteTestCpp98::tempAssign(dtc98);
+
 	// In C++11, deleted function will fail to compile if it tries to copy DeleteTestCpp11 objects.
 	// That's an improvement over the C++98 behavior, where such improper usage wouldn't be diagnosed until link-time.
 	DeleteTestCpp11 dtc11;
