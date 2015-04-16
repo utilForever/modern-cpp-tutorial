73cb8770ad268c629bb865326ade808f829779a2
utilForever <utilForever@gmail.com>
2015-04-17 00:07:03 +0900

    Update Comment

--- ModernCpp/nullptr.cpp
+++ ModernCpp/nullptr.cpp
@@ -1,3 +1,10 @@
+/*************************************************************************
+> File Name: nullptr.cpp
+> Author: Chan-Ho Chris Ohk
+> E-mail: utilForever@gmail.com, utilForever@kaist.ac.kr
+> Created Time: 2015/4/16
+> Personal Blog: https://github.com/utilForever
+************************************************************************/
 #include <iostream>
 
 void func(char*);
