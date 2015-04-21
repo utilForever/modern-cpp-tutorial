2cc791e93c1178eea4640870e85beeb8891ad3f7
utilForever <utilForever@gmail.com>
2015-04-21 09:34:30 +0900

    Update Comment

---
+++ ModernCpp/ruleOfFive.cpp
@@ -0,0 +1,7 @@
+/*************************************************************************
+> File Name: scopedEnum.cpp
+> Author: Chan-Ho Chris Ohk
+> E-mail: utilForever@gmail.com, utilForever@kaist.ac.kr
+> Created Time: 2015/4/19
+> Personal Blog: https://github.com/utilForever
+************************************************************************/
