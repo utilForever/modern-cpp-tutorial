f44961ee77181c4679cc7e1bb36dd7a987e7265e
utilForever <utilForever@gmail.com>
2015-04-24 02:24:02 +0900

    Update Comment

--- ModernCpp/ruleOfFive.cpp
+++ ModernCpp/ruleOfFive.cpp
@@ -2,6 +2,6 @@
 > File Name: ruleOfFive.cpp
 > Author: Chan-Ho Chris Ohk
 > E-mail: utilForever@gmail.com, utilForever@kaist.ac.kr
-> Created Time: 2015/4/22
+> Created Time: 2015/4/24
 > Personal Blog: https://github.com/utilForever
 ************************************************************************/
