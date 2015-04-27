89355b0be3bec75ea8cd6ff9783a46316e994709
utilForever <utilForever@gmail.com>
2015-04-27 10:19:39 +0900

    Create "default" Keyword Example

---
+++ ModernCpp/Classes/defaultKeyword.cpp
@@ -0,0 +1,8 @@
+/*************************************************************************
+> File Name: defaultKeyword.cpp
+> Author: Chan-Ho Chris Ohk
+> E-mail: utilForever@gmail.com, utilForever@kaist.ac.kr
+> Created Time: 2015/4/27
+> Personal Blog: https://github.com/utilForever
+> References: http://scottmeyers.blogspot.fr/2014/03/a-concern-about-rule-of-zero.html
+************************************************************************/
