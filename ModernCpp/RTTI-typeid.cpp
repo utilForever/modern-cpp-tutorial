e9dc7f80570de51f4b4dc7a8c83116625ce6429d
Chris Ohk <utilForever@gmail.com>
2015-07-15 14:02:40 +0900

    Create RTTI typeid Example

---
+++ ModernCpp/RTTI-typeid.cpp
@@ -0,0 +1,36 @@
+/*************************************************************************
+> File Name: RTTI-typeid.cpp
+> Author: Chan-Ho Chris Ohk
+> E-mail: utilForever@gmail.com, utilForever@kaist.ac.kr
+> Created Time: 2015/7/15
+> Personal Blog: https://github.com/utilForever
+************************************************************************/
+#include <iostream>
+#include <typeinfo>
+
+class Animal
+{
+	virtual void something() { }
+};
+
+class Dog : public Animal { };
+class Bird : public Animal { };
+
+void speak(const Animal& animal)
+{
+	if (typeid(animal) == typeid(Dog&))
+		std::cout << "Woof!" << std::endl;
+	else if (typeid(animal) == typeid(Bird&))
+		std::cout << "Chirp!" << std::endl;
+}
+
+int main()
+{
+	Dog dog;
+	Bird bird;
+
+	Animal& animal = dog;
+	speak(animal);
+	animal = bird;
+	speak(bird);
+}
