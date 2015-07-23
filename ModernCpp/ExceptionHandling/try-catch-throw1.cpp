460f05d3cfa04a101d71b44fd896a47521bebe15
Chris Ohk <utilForever@gmail.com>
2015-07-23 10:58:09 +0900

    Create Try-Catch-Throw Examples

---
+++ ModernCpp/ExceptionHandling/try-catch-throw1.cpp
@@ -0,0 +1,32 @@
+/*************************************************************************
+> File Name: try-catch-throw1.cpp
+> Author: Chan-Ho Chris Ohk
+> E-mail: utilForever@gmail.com, utilForever@kaist.ac.kr
+> Created Time: 2015/7/17
+> Personal Blog: https://github.com/utilForever
+************************************************************************/
+#include <iostream>
+
+int Divide(int num, int den)
+{
+	if (den == 0)
+		throw std::invalid_argument("Divide by zero");
+	return num / den;
+
+}
+
+int main()
+{
+	try
+	{
+		std::cout << Divide(5, 2) << std::endl;
+		std::cout << Divide(10, 0) << std::endl;
+		std::cout << Divide(3, 3) << std::endl;
+	}
+	catch (const std::invalid_argument& e)
+	{
+		std::cout << "Caught exception: " << e.what() << std::endl;
+	}
+
+	return 0;
+}
---
+++ ModernCpp/ExceptionHandling/try-catch-throw2.cpp
@@ -0,0 +1,56 @@
+/*************************************************************************
+> File Name: try-catch-throw2.cpp
+> Author: Chan-Ho Chris Ohk
+> E-mail: utilForever@gmail.com, utilForever@kaist.ac.kr
+> Created Time: 2015/7/17
+> Personal Blog: https://github.com/utilForever
+************************************************************************/
+#include <iostream>
+#include <string>
+#include <vector>
+#include <fstream>
+
+void readFile(const std::string& fileName, std::vector<int>& dest) throw (std::invalid_argument, std::runtime_error)
+{
+	std::ifstream istr;
+	int temp;
+	istr.open(fileName.c_str());
+	if (istr.fail())
+		// 파일 열기 실패에 따른 익셉션
+		throw std::invalid_argument("");
+	// 값을 하나씩 읽어서 vector에 저장
+	while (istr >> temp)
+		dest.push_back(temp);
+	if (istr.eof())
+		istr.close();
+	else {
+		// 알 수 없는 오류에 의한 익셉션
+		istr.close();
+		throw std::runtime_error("");
+	}
+}
+
+int main()
+{
+	std::vector<int> myInts;
+	const std::string fileName = "numbers.txt";
+
+	try
+	{
+		readFile(fileName, myInts);
+	}
+	catch (const std::invalid_argument& e)
+	{
+		std::cerr << "Unable to open file " << fileName << std::endl;
+		return 1;
+	}
+	catch (const std::runtime_error& e)
+	{
+		std::cerr << "Error reading file " << fileName << std::endl;
+		return 1;
+	}
+
+	for (size_t i = 0; i < myInts.size(); i++)
+		std::cout << myInts[i] << " ";
+	std::cout << std::endl;
+}
