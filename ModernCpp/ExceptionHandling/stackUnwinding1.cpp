92d14dd2db7866ad5b656630dd681ea6b8b1b198
Chris Ohk <utilForever@gmail.com>
2015-07-23 10:57:59 +0900

    Create Stack Unwinding Examples

---
+++ ModernCpp/ExceptionHandling/stackUnwinding1.cpp
@@ -0,0 +1,26 @@
+/*************************************************************************
+> File Name: stackUnwinding1.cpp
+> Author: Chan-Ho Chris Ohk
+> E-mail: utilForever@gmail.com, utilForever@kaist.ac.kr
+> Created Time: 2015/7/17
+> Personal Blog: https://github.com/utilForever
+************************************************************************/
+
+#include <iostream>
+
+void f1() { throw 0; }
+void f2() { f1(); }
+void f3() { f2(); }
+void f4() { f3(); }
+
+int main()
+{
+	try
+	{
+		f4();
+	}
+	catch (int e)
+	{
+		std::cout << e << std::endl;
+	}
+}
---
+++ ModernCpp/ExceptionHandling/stackUnwinding2.cpp
@@ -0,0 +1,56 @@
+/*************************************************************************
+> File Name: stackUnwinding2.cpp
+> Author: Chan-Ho Chris Ohk
+> E-mail: utilForever@gmail.com, utilForever@kaist.ac.kr
+> Created Time: 2015/7/17
+> Personal Blog: https://github.com/utilForever
+************************************************************************/
+
+#include <iostream>
+#include <string>
+#include <fstream>
+#include <memory>
+
+void funcOne() throw(std::exception);
+void funcTwo() throw(std::exception);
+
+int main()
+{
+	try
+	{
+		funcOne();
+	}
+	catch (const std::exception& e)
+	{
+		std::cerr << "Exception caught!" << std::endl;
+		return 1;
+	}
+	return 0;
+}
+
+void funcOne() throw(std::exception)
+{
+	std::string str1;
+	std::unique_ptr<std::string> str2(new std::string(""));
+	funcTwo();
+
+	//string* str2 = new string();
+	//try
+	//{
+	//	funcTwo();
+	//}
+	//catch (...) {
+	//	delete str2;
+	//	throw;	// Re-send exception
+	//}
+	//
+	//delete str2;
+}
+
+void funcTwo() throw(std::exception)
+{
+	std::ifstream istr;
+	istr.open("fileName");
+	throw std::exception();
+	istr.close();
+}
